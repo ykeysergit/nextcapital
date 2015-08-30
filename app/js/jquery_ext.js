74dda42f96658be45dea01b3009444ce0e18e1d4
Yev <tangeroknight@gmail.com>
2015-08-30 02:58:54 -0500

    setup karma added unit test for TodoComparator

--- app/js/jquery_ext.js
+++ app/js/jquery_ext.js
@@ -1,7 +1,7 @@
 (function($){
   $.isBlank = function(obj){
     return !obj || 
-    $.isEmptyObject(obj) ||
+    ($.type(obj)==='object' &&  $.isEmptyObject(obj)) ||
     ($.type(obj)==='array' && obj.length==0) ||
     ($.isNumeric(obj) && obj==0) ||
     ($.type(obj)==='string' && $.trim(obj).length==0);
